c3940bbac66a9b5ab3929365255b8718c1354b21
smosgr <stel.moschos@gmail.com>
2018-02-20 14:42:17 +0000

    fixed calculator class

--- src/main/java/CompoundInterestCalc.java
+++ src/main/java/CompoundInterestCalc.java
@@ -1,49 +1,21 @@
-import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class CompoundInterestCalc {
 
-    static int LENDING_PERIOD_IN_MONTHS = 36;
-    double amount;
-    double rate;
-    double year;
+    public void calculateInterest(double principal, int years, double rate) { //principal= 1000, years=3, rate = 0.054444285714286;
 
+        double compoundInterest = principal * Math.pow((1 + rate), years);
+        double totalRepayment = compoundInterest * principal;
 
-    CompoundInterestCalc() {
+        printOutput(principal, rate, compoundInterest, totalRepayment);
     }
 
-    public void calculateInterest() {
-
-        System.out.println("This program, with user input, computes interest.\n" +
-                "It allows for multiple computations.\n" +
-                "User will input initial cost, interest rate and number of years.");
-
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("What is the initial cost?");
-        amount = keyboard.nextDouble();
-
-        System.out.println("What is the interest rate?");
-//        rate = keyboard.nextDouble();
-        rate = 0.054444285714286;
-
-        System.out.println("How many years?");
-        year = keyboard.nextInt(); // static int LENDING_PERIOD_IN_MONTHS = 36; aka 3 years.
-
-
-        for (int x = 1; x < year; x++) {
-            String go = "n";
-            System.out.println("For " + year + " years an initial " + amount
-                    +" cost compounded at a rate of " + rate + " will grow to " + amount);
-
-
-            amount = amount * Math.pow(1.0 + rate, year);
-
-
-            do {
-                System.out.println("Continue Y/N");
-                go = keyboard.nextLine();
-            } while (go.equals("Y") || go.equals("y"));
-
-        }
+    public void printOutput(double principal, double rate, double compoundInterest, double totalRepayment) {
+        System.out.println("/n");
+        System.out.println("Requested amount: " + "£" + principal);
+        System.out.println("Rate: " + new DecimalFormat("##.#").format(rate) + "%");
+        System.out.println("Monthly repayment: " + "£" + 345); //<--------CHANGE THAT
+        System.out.println("Total repayment: " + "£" + totalRepayment); //<-------- CHANGE THAT
+        System.out.println("The Compound Interest is : " + compoundInterest);
     }
 }
